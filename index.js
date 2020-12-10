1c59fdf897c3d9a0c48ce7e135d6e8173cafaede
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2020-12-09 23:10:58 -0500

    first set of content

---
+++ index.js
@@ -0,0 +1,51 @@
+var ws_uri = "ws://[http://127.0.0.1]:5500";
+var websocket = new WebSocket(ws_uri);
+
+document.getElementById("chat-form").addEventListener("submit", (event) => {
+	event.preventDefault();
+
+    var message_element = document.getElementsByTagName("input")[0];
+    var message = message_element.value;
+
+    if(message.toString().length) {
+        var data = {
+            type: "message",
+            username: "You",
+            message: message
+        };
+        websocket.send(JSON.stringify(data));
+        message_element.value = "";
+    }
+}, false);
+
+
+//on websocket open: 
+websocket.onopen = (event) => {
+    messageAdd('<div class="message green">You have entered the chat room.</div>');
+
+};
+
+//on websocket closes
+websocket.onclose = (event) => {
+    MessageAdd('<div class="message blue">You have been disconnected.</div>');
+};
+
+//When there is an error
+websocket.onerror = (event) => {
+    messageAdd('<div class="message red">Connection to chat failed.</div>');
+};
+
+websocket.onmessage = (event) => {
+    var data = JSON.parse(event.data);
+
+    if(data.type == "message") {
+        messageAdd('<div class="message">' + data.username + ': ' + data.message + '</div>');
+    }
+};
+
+function MessageAdd(message) {
+	var chat_messages = document.getElementById("chat-messages");
+
+	chat_messages.insertAdjacentHTML("beforeend", message);
+	chat_messages.scrollTop = chat_messages.scrollHeight;
+}
